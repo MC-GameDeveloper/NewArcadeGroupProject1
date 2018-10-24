dd1bd0ce33ecca6649d1f5b4902e0c5be29c49be
MT_CENT <mtc9417@gmail.com>
2018-10-24 12:19:11 -0400

    HingeJoint Game

---
+++ HingeJoint/Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+
+	public float followSpeed;
+
+	// Use this for initialization
+
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float superDistance = Vector2.Distance (transform.position, player.transform.position);
+
+		transform.position = Vector3.Lerp (transform.position, player.transform.position, followSpeed * superDistance * Time.deltaTime);
+	}
+}
---
+++ HingeJoint/Assets/Scripts/GameController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+public class GameController : MonoBehaviour {
+
+	private SerialPort stream = new SerialPort("\\\\.\\COM11", 9600);
+	private Vector3 temp;
+	// Use this for initialization
+	void Start () {
+		stream.Open();
+		stream.ReadTimeout = 25;
+		StartCoroutine (readString ());
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	IEnumerator readString() {
+
+		while (true) {
+
+			if (stream.IsOpen) {
+				try
+				{
+
+					string value = stream.ReadLine();
+					string[] values = value.Split(',');
+					//Debug.Log(values[0]);
+					Debug.Log(float.Parse(values[0]));
+					temp = transform.rotation.eulerAngles;
+					temp.z = Mathf.Clamp(float.Parse(values[0]),-25,25);
+					transform.rotation = Quaternion.Euler(temp);
+
+
+
+				}
+				catch (System.Exception) {
+
+				}
+
+			}
+
+			yield return null;
+
+		}
+
+	}
+}
