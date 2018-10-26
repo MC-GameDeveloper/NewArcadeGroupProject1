fc1a11d348229214f51deaec59f789a020f66802
MT_CENT <mtc9417@gmail.com>
2018-10-26 15:16:56 -0400

    step2

--- HingeJoint/Assets/Scripts/GameController.cs
+++ HingeJoint/Assets/Scripts/GameController.cs
@@ -3,51 +3,100 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO.Ports;
 
+
 public class GameController : MonoBehaviour {
 
-	private SerialPort stream = new SerialPort("\\\\.\\COM11", 9600);
-	private Vector3 temp;
+	private SerialPort stream1 = new SerialPort("\\\\.\\COM17", 9600);
+	//private SerialPort stream2 = new SerialPort("\\\\.\\COM11", 9600);
+	private Vector3 temp1;
+	private Vector3 temp2;
+
+	public GameObject player1;
+	public GameObject player2;
+
+	public GameObject targetRef;
+	private GameObject targetClone;
+
+	public GameObject ballRef;
+	private GameObject ballClone;
+
 	// Use this for initialization
 	void Start () {
-		stream.Open();
-		stream.ReadTimeout = 25;
-		StartCoroutine (readString ());
-
-
+		stream1.Open();
+		stream1.ReadTimeout = 25;
+		//stream2.Open();
+		//stream2.ReadTimeout = 25;
+		StartCoroutine (readString1 ());
+		//StartCoroutine (readString2 ());
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		float randomY = Random.Range (10f, 20f);
+		float randomX = Random.Range (-11f, 4.5f);
+		Vector3 randomSpawnPos = new Vector3 (randomX, randomY, 0);
+		if (targetClone == null) {
+			targetClone = Instantiate (targetRef, randomSpawnPos, Quaternion.identity);
+		}
+
+		Vector3 ballSpawn = new Vector3 (-2.5f, 6f, 0f);
+		if (ballClone == null) {
 		
+			ballClone = Instantiate (ballRef, ballSpawn, Quaternion.identity);
+		}
 	}
-	IEnumerator readString() {
+
+	//read serial for Player1
+	IEnumerator readString1() {
 
 		while (true) {
 
-			if (stream.IsOpen) {
+			if (stream1.IsOpen) {
 				try
 				{
 
-					string value = stream.ReadLine();
-					string[] values = value.Split(',');
-					//Debug.Log(values[0]);
-					Debug.Log(float.Parse(values[0]));
-					temp = transform.rotation.eulerAngles;
-					temp.z = Mathf.Clamp(float.Parse(values[0]),-25,25);
-					transform.rotation = Quaternion.Euler(temp);
-
-
+					string value = stream1.ReadLine();
+					Debug.Log(float.Parse(value));
+					temp1 = transform.rotation.eulerAngles;
+					temp1.z = Mathf.Clamp(float.Parse(value),-15,15);
+					player1.transform.rotation = Quaternion.Euler(temp1);
+			
 
 				}
 				catch (System.Exception) {
-
 				}
 
 			}
 
 			yield return null;
-
 		}
+	}
 
+	//read serial for Player2
+	IEnumerator readString2() {
+
+		while (true) {
+
+			//if (stream2.IsOpen) 
+			{
+				try
+				{
+
+					string value = stream1.ReadLine();
+					string[] values = value.Split(',');
+					//Debug.Log(values[0]);
+					Debug.Log(float.Parse(values[0]));
+					temp1 = transform.rotation.eulerAngles;
+					temp1.z = Mathf.Clamp(float.Parse(values[0]),-15,15);
+					player2.transform.rotation = Quaternion.Euler(temp1);
+
+				}
+				catch (System.Exception) {
+
+				}
+			}
+
+			yield return null;
+		}
 	}
 }
